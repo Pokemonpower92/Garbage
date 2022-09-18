83ad4f7c88b9c01c38efd2b4aa7a939a51c0ab1d
Poochofpain <pokemonpower92@gmail.com>
2022-09-17 22:15:28 -0600

    Removing extraneous files.

--- build/lib/config/tilemap_codes.py
+++
@@ -1,6 +0,0 @@
-from config import resource_paths
-
-TILE_MAP_A_MAPPING = {"b": resource_paths.TIILEMAP_B}
-TILE_MAP_B_MAPPING = {"a": resource_paths.TIILEMAP_A}
-
-tilemap_mapping = {"a": TILE_MAP_A_MAPPING}
