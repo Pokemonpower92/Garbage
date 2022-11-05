d3889cbee9c8800bdbb296702486060c903f9624
Poochofpain <pokemonpower92@gmail.com>
2022-11-05 11:46:07 -0600

    Rectifying the main game loop.

--- gamestate/loop/title_screen_loop.py
+++ gamestate/loop/title_screen_loop.py
@@ -16,6 +16,7 @@ class TitleScreenLoop(Loop):
     def __init__(self):
         super().__init__()
         self.load_assets()
+        self.can_interact = False
 
     def draw(self):
         self.menu.draw_background()
@@ -29,17 +30,26 @@ class TitleScreenLoop(Loop):
         pygame.display.update()
 
     def run(self):
-
+        self.time_since_loop_started = pygame.time.get_ticks()
         self.running = True
         while self.running:
+            self.cooldown()
             self.event_loop()
             self.update()
             self.draw()
 
+    def cooldown(self):
+        current_time = pygame.time.get_ticks()
+        delta_time = current_time - self.time_since_loop_started
+
+        if delta_time >= 500:
+            self.can_interact = True
+
     def event_loop(self):
         listen.event_loop()
 
-        pressed_keys = pygame.key.get_pressed()
-
-        if pressed_keys[pygame.K_RETURN]:
-            MainMenuLoop().run()
+        if self.can_interact:
+            self.menu.check_events()
+            pressed_keys = pygame.key.get_pressed()
+            if pressed_keys[pygame.K_RETURN]:
+                MainMenuLoop().run()
--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from gamestate.loop.title_screen_loop import TitleScreenLoop
-from gamestate.loop.pause_menu_loop import PauseMenuLoop
-from gamestate.loop.main_menu_loop import MainMenuLoop
 import pygame
 
 if __name__ == "__main__":
     pygame.init()
-    MainMenuLoop().run()
+    TitleScreenLoop().run()
