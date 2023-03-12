7690f3a788dd951db8711184496c26867dcadb82
jcarmac3 <pokemonpower92@gmail.com>
2023-03-12 11:47:50 -0600

    Need to patch pygame.display.

--- assets/test/test_main_menu_assets.py
+++ assets/test/test_main_menu_assets.py
@@ -3,14 +3,16 @@ import pygame
 import logging
 
 from assets.main_menu_assets import MainMenuAssets
+from unittest.mock import patch
 
 
+@patch("pygame.display")
 class TestMainMenuAssets(unittest.TestCase):
 
     def setUp(self) -> None:
         pygame.init()
 
-    def test_create_asset(self):
+    def test_create_asset(self, mock_pytest):
         asset = MainMenuAssets()
         assert asset
 
