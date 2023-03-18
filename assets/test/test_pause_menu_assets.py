3123c288f6116dd78222a5e639a538c03741feab
jcarmac3 <pokemonpower92@gmail.com>
2023-03-18 10:45:34 -0600

    Need to patch mouse events.

--- assets/test/test_pause_menu_assets.py
+++ assets/test/test_pause_menu_assets.py
@@ -7,20 +7,21 @@ import logging
 from assets.pause_menu_assets import PauseMenuAssets
 
 @patch("pygame.display")
+@patch("pygame.mouse.get_pos", return_value=(0,0))
 class TestPauseMenuAssets(unittest.TestCase):
 
     def setUp(self) -> None:
         pygame.init()
 
-    def test_create_asset(self, mock_pygame):
+    def test_create_asset(self, mock_display, mock_mouse):
         asset = PauseMenuAssets()
         assert asset
 
-    def test_load_assets(self, mock_pygame):
+    def test_load_assets(self, mock_display, mock_mouse):
         asset = PauseMenuAssets()
         asset.load_assets()
 
-    def test_check_events(self, mock_pygame):
+    def test_check_events(self, mock_display, mock_mouse):
         asset = PauseMenuAssets()
         asset.load_assets()
         asset.check_events()
